2fb0aff540f92ab7361cee56b81ca52ddf72ec6b
Ilshat <dlyaGitHuba@mail.ru>
2022-05-09 23:33:18 +0500

    update

--- index.js
+++ index.js
@@ -22,9 +22,12 @@ app.use('/api/cart/', require('./routes/cart'))
 app.use('/api/orders/', require('./routes/orders'))
 app.use('/api/comments/', require('./routes/comments'))
  
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client', 'build')))
-    app.use('*', express.static(path.join(__dirname, "client", "build")))
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "client/build")));
+
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    })
 }
 
 mongoose.connect(keys.MONGO_URI, {
