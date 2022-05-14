030cd01958c8bbf90de652dfe28dc1fae1500d19
Ilshat <dlyaGitHuba@mail.ru>
2022-05-14 17:56:12 +0500

    firebase config update

--- client/src/helpers/firebase.js
+++ client/src/helpers/firebase.js
@@ -2,13 +2,13 @@ import firebase from "@firebase/app";
 import "@firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAQa1YbFOXAlTZG2n8ZTENZLHI3aAyhol4",
-    authDomain: "photoprint-db532.firebaseapp.com",
-    projectId: "photoprint-db532",
-    storageBucket: "photoprint-db532.appspot.com",
-    messagingSenderId: "937877443867",
-    appId: "1:937877443867:web:b49b11b7393a94fe9c2fbe",     
-    measurementId: "G-6Z48ER252Q"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,     
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
