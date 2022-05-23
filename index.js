5e950ab997d7192d45d460ef62d6628ebadc6852
Ilshat <dlyaGitHuba@mail.ru>
2022-05-23 21:07:04 +0500

    dotenv added

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const path = require('path')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const app = express();
 
-const keys = require('./config/keys')
-const PORT = process.env.PORT || 5000
- 
-app.use(express.json())
-app.use('/api/auth/', require('./routes/auth'))
-app.use('/api/goods/', require('./routes/goods'))
-app.use('/api/cart/', require('./routes/cart'))
-app.use('/api/orders/', require('./routes/orders'))
-app.use('/api/comments/', require('./routes/comments'))
- 
+const keys = require("./config/keys");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use("/api/auth/", require("./routes/auth"));
+app.use("/api/goods/", require("./routes/goods"));
+app.use("/api/cart/", require("./routes/cart"));
+app.use("/api/orders/", require("./routes/orders"));
+app.use("/api/comments/", require("./routes/comments"));
+
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "client/build", "index.html"));
-    })
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
 }
 
-mongoose.connect(keys.MONGO_URI, {
+mongoose
+  .connect(keys.MONGO_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
-}).then(() => app.listen(PORT, () => console.log(`The server is running on port ${PORT}`)))
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`The server is running on port ${PORT}`))
+  );
--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,28 +1,27 @@
 const nodemailer = require("nodemailer");
-const keys = require("../config/keys");
 
 const sendEmail = (options) => {
   console.log("EMAIL_HOST");
-  console.log(keys.EMAIL_HOST);
+  console.log(process.env.EMAIL_HOST);
 
   const transporter = nodemailer.createTransport({
-    host: keys.EMAIL_HOST,
-    port: keys.EMAIL_PORT,
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
     secure: true,
     auth: {
-      user: keys.EMAIL_USERNAME,
-      pass: keys.EMAIL_PASSWORD,
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
   const mailOptions = {
-    from: keys.EMAIL_FROM,
+    from: process.env.EMAIL_FROM,
     to: options.to,
     subject: options.subject,
     html: options.text,
   };
   console.log("EMAIL_HOST");
-  console.log(keys.EMAIL_HOST);
+  console.log(process.env.EMAIL_HOST);
 
   transporter.sendMail(mailOptions, function (err, info) {
     if (err) console.log(err);
